66a7d6d3692aa134264423eb227cdc2bafea086a
Mahad-Nawaz_Khan <mahadnawaz2006@gmail.com>
2024-06-16 20:09:51 +0500

    First Commit

---
+++ index.ts
@@ -0,0 +1,52 @@
+#! /usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer'
+
+console.log(chalk.cyanBright('\nWelcome to Number Guessing game by Mahad Nawaz Khan\n'));
+
+
+let exit: boolean = false;
+while(!exit){
+
+    const ramdom: number = Math.floor(Math.random()* 10);
+
+    const choice = await inquirer.prompt(
+        {
+          type: 'list',
+          name: 'choice',
+          message: chalk.italic.rgb(255,165,0) ('Main Menu\n\t'),
+          choices: ['PLAY', 'EXIT'],
+          prefix: "      "
+        }
+    )
+
+    if (choice.choice == 'EXIT') {
+
+        console.log(chalk.yellow('Thank You For Running My Program \n'));
+        console.log('='.repeat(70)+'\n');
+        exit = true;
+        process.exit();
+
+    } else if (choice.choice == 'PLAY') {
+    
+        const question = await inquirer.prompt(
+            {
+              type: 'input',
+              name: 'input',
+              message: 'Guess a number from 1 to 10',
+              prefix: chalk.green('\n>')
+            }
+        )
+
+        if(question.input == ramdom) {
+
+            console.log (chalk.bold.green('\nCongratulation You Guessed The Right Number\n'));
+
+        } else {
+
+            console.log (chalk.bold.red('\nSorry You Guessed Wrong Number\n'));
+        }
+    }
+    console.log('='.repeat(70)+'\n');
+}
---
+++ reame.md
@@ -0,0 +1,3 @@
+#Ramadan Coding Nights 2024
+
+A Governer Sindh I.T Initiative.
